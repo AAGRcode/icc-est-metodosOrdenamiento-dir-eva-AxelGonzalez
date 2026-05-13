de9aae5dddd3be7582927c9145e3834b3c3de1b4
Your Name <you@example.com>
2026-05-13 13:58:20 +0000

    Ya acabe por fin

--- src/controllers/CarController.java
+++ src/controllers/CarController.java
@@ -3,5 +3,22 @@ package controllers;
 import models.Car;
 
 public class CarController {
-  public void sortByName(Car[] cars){}
+  // burbuja avanzado
+  public void sortByName(Car[] cars){
+    for(int i=0; i<cars.length; i++){
+      boolean swap = false;
+      for(int j=0; j<cars.length-1-i; j++){
+        if(cars[j].getName().compareTo(cars[j+1].getName())>0){
+          Car aux = cars[j];
+          cars[j] = cars[j+1];
+          cars[j+1] = aux;
+          swap = true;
+
+        }
+      }
+      if(!swap){
+        break;
+      }
+    }
+  }
 }
